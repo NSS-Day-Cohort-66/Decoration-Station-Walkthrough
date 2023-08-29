979b701ce1d1f67ef6c6fdcb33f162e6fda4fd23
Valerie Freeman <valerah7@gmail.com>
2023-08-29 09:37:07 -0500

    begin

--- src/components/DecorationStation.js
+++
@@ -1,3 +0,0 @@
-export const DecorationStation = () => {
-  return <></>
-}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { DecorationStation } from './components/DecorationStation'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import { App } from "./App.js"
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <DecorationStation />
-  </React.StrictMode>
-)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<App />)
