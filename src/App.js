86789cb1fcad54576f6b7e34582f404fcdccc61f
Valerie Freeman <valerah7@gmail.com>
2023-09-12 09:27:50 -0500

    item details

--- src/App.js
+++ src/App.js
@@ -19,26 +19,11 @@ export const App = () => {
       >
         <Route index element={<ItemsList />} />
         <Route path="items">
-          <Route path=":itemId" element={<ItemDetails />} />{" "}
+          <Route path=":itemId" element={<ItemDetails />} />
+          <Route path=":otherThing/thing" element={<>Does this work?</>} />
           {/* This Route will render when the url is localhost:3000/items/[some id] */}
           <Route path="new" element={<NewDecorationForm />} />
         </Route>
-
-        {/* Fun routes for learning */}
-        <Route
-          path="hello"
-          element={
-            <>
-              <h1>Hello </h1>
-              <Outlet />
-            </>
-          }
-        >
-          <Route path="kevin" element={<h2>Kevin!</h2>} />
-          <Route path="sylvia" element={<h2>Sylvia!</h2>} />
-          <Route path="helen" element={<h2>Helen!</h2>} />
-        </Route>
-        <Route path="bye" element={<h2>Bye Bye</h2>} />
       </Route>
     </Routes>
   )
--- src/services/itemsService.js
+++ src/services/itemsService.js
@@ -13,3 +13,9 @@ export const postItem = (item) => {
     body: JSON.stringify(item),
   })
 }
+
+export const getItemById = (id) => {
+  return fetch(
+    `http://localhost:8088/items/${id}?_expand=season&_expand=category`
+  ).then((res) => res.json())
+}
